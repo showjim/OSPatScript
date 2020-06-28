9c31122f97494434c081e15d3880af545c1cb2b7
Jerry <zhouchao486@gmail.com>
2020-06-28 13:42:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+'''
+Create @ 2017/03/29
+Author: zhouchao
+'''
+
+
+import re
+import os
+
+
+def main():
+    dir = cur_file_dir()
+    pinmap_dir = dir + r'\Pinmap.txt'
+    output_dir = dir + r'\output.atp'
+    pattern = re.compile(r'(?<=OS_VSS\s)\w+')
+    # print(pattern.findall('OS_VSS	ooo\n   OS_VSS	qqq'))
+    with open(output_dir, mode='w') as output_file:
+        output_file.write('opcode_mode = single;\nimport tset timeplate_1;\n')
+        with open(pinmap_dir, mode='r') as f:  # , encoding='UTF-8'
+            lines = f.readlines()
+            pinlist = pattern.findall(' '.join(lines))
+        output_file.write('vm_vector	($tset,' + ','.join(pinlist) + ')\n')
+        output_file.write('{\nstart_label OS_PAT_start:\n')
+        pin_cnt = len(pinlist)
+        output_file.write('> timeplate_1 ' + '0 ' *
+                          pin_cnt + ';\n')  # first all 0 row
+        for i in range(pin_cnt):  # Row index
+            tmpstr = '> timeplate_1 '
+            for j in range(pin_cnt):  # Column	index
+                if i == j:
+                    tmpstr += 'M '
+                else:
+                    tmpstr += '0 '
+            output_file.write(tmpstr + ';\n')
+        output_file.write('}')
+
+
+def cur_file_dir():
+    path = os.getcwd()
+    return path
+
+
+if __name__ == '__main__':
+    main()
